2f136dc93817bc0a5b787bcb22bdd3333eba9803
carldanley <carldanley@gmail.com>
2013-03-02 16:09:15 -0500

    removed some leftover code idea that sucked.

--- event-manager.js
+++ event-manager.js
@@ -90,11 +90,7 @@
 		 * Performs an filter if it exists. You should only ever pass 1 argument to be filtered. The only rule is that
 		 * the first argument must always be the filter.
 		 */
-		SELF.applyFilter = function( /* filter, arg1 */ ) {
-			var args = Array.prototype.slice.call( arguments );
-			var filter = args.shift();
-			var param = args.shift();
-
+		SELF.applyFilter = function( filter, param ) {
 			if( _validateNamespace( filter ) === false ) {
 				return SELF;
 			}
